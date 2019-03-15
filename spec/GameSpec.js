590308eca3a0593171bfb95bc80379dce16a9632
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-15 03:18:04 +0000

    calculation of total score for game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -19,9 +19,9 @@ var game;
   it('knows which frame it is in after initialization', function(){
     game.roll(4);
     game.roll(1);
-    // game.getTotalScore();
+    game.getTotalScore()
     expect(game._currentFrameNumber).toEqual(2);
-    expect(game.getTotalScore()).toEqual(5);
+    expect(game._totalScoreForGame).toEqual(5);
   });
 
   it('knows which frame it is in after multiple rolls', function(){
@@ -33,7 +33,5 @@ var game;
     game.roll(1);
     game.getTotalScore();
     expect(game._currentFrameNumber).toEqual(4);
-    expect(game.getTotalScore()).toEqual(15);
-    // expect(game._totalScoreForGame).toEqual(15);
   });
 });
